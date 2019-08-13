60d7809073f81180d0b688a849f4b42042d95d98
Ketho <sghs17@outlook.com>
2019-08-13 13:55:53 +0200

    Hide the blackout background

--- MapFader.lua
+++ MapFader.lua
@@ -2,3 +2,6 @@ local MIN_ALPHA = .4
 
 -- we dont care about cvar mapFade and mousehovering over the worldmapframe
 PlayerMovementFrameFader.AddDeferredFrame(WorldMapFrame, MIN_ALPHA, 1.0, .5)
+
+-- hide the blackout background
+WorldMapFrame.BlackoutFrame:Hide()
